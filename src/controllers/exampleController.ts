9b3e82f5beebf9310444b755ba4bf6b83ca39670
Yeison Andres Hernandez <desarrollojava@ada.co>
2024-07-29 02:24:11 -0500

    delete

--- src/controllers/exampleController.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from 'express';
-
-export const exampleController = (req: Request, res: Response) => {
-  res.json({ message: 'This is an example controller response' });
-};
